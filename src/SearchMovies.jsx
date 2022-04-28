d12e5134126c764b63c8718da3828ec8c541d74d
TJacka <vegozzy@gmail.com>
2022-04-28 12:18:40 -0700

    Initial commit

---
+++ src/SearchMovies.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import MovieCard from "./MovieCard";
+
+export default function SearchMovies() {
+    
+    const [query, setQuery] = React.useState("")
+    const [movies, setMovies] = React.useState([])
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+            
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=04bd14764b1b552d6250e990429e33c8&language=en-US&query=${query}&page=1&include_adult=false`;
+            
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            setMovies(data.results);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+    return (
+        <div>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input 
+                    className="input" 
+                    type="text" 
+                    name="query" 
+                    placeholder="i.e. Jurassic Park" 
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>  
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import SearchMovies from './SearchMovies';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
+      </div>
+    );
+  }
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Main />);
